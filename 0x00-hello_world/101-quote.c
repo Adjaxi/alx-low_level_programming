e889cec240fabd3e27f99a0be5e1a2e63dfe887d
Brian Korir <briankorir026@gmail.com>
2023-06-15 17:53:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #!/bin/bash
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
+ * main - prints "and that piece of artis useful" -Dora Korpar, 2015-10-19\n", 59
+ * followed by a new line, to the standard error.
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2'
-			"and that piece of artis useful\" - Dora Korpar. 2015-10-19\n", $9);
+
+	write(2' "and that piece of artis useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1)
 }
