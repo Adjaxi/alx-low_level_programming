ddfe0c5dc6f5853b7fae7fc529d91491b5e11bb8
Brian Korir <briankorir026@gmail.com>
2023-06-15 17:22:56 -0700

    My Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main - Prints Programming is like building a multilingual puzzle
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 
 {
-        printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
