69bccae46ced0d675bc320917a111b554ae95bb6
Brian Korir <briankorir026@gmail.com>
2023-06-15 17:57:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#!/bin/bash
 #include <unistd.h>
+
 /**
  * main - prints "and that piece of artis useful" -Dora Korpar, 2015-10-19\n", 59
  * followed by a new line, to the standard error.
  * Return: Always 1 (Success)
  */
-int main(void)
-{
 
+int main(void)
+
+{
 	write(2' "and that piece of artis useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1)
 }
