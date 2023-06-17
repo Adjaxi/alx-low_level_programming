e7f9d01f7b3224400e5ffa59d4ee0ac5ead9b745
Brian Korir <briankorir026@gmail.com>
2023-06-16 18:37:00 -0700

    print the alphabet in lowercase,

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,24 +2,20 @@
 
 /**
  * main - Prints the alphabet in lowercase, and then in uppercase.
- * Description: 'A program that prints the alphabet in lowercase.'
+ *
+ * Description: A program that prints the alphabet in lowercase and uppercase.
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char lowercase = 'a';
-	char uppercase = 'A';
-	while (lowercase <= 'z')
-	{
-		putchar(lowercase);
-		lowercase++;
-	}
-	while (uppercase <= 'Z')
-	{
-		putchar(uppercase);
-		uppercase++;
-	}
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
 	putchar('\n');
 	return (0);
 }
