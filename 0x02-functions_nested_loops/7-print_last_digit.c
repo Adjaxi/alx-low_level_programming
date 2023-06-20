ed4603bd45ce2eaca8fcd2810cd73996ae8f4447
Brian Korir <briankorir026@gmail.com>
2023-06-20 16:22:03 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,17 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	intl;
+
+	l = n % 10;
+	if (l < 0)
+	{
+	_putchar(-l + 48);
+	return (-l);
+	}
+	else
+	{
+		putchar(l + 48);
+		return (l);
+	}
 }
