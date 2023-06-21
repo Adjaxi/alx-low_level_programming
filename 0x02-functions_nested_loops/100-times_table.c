3b1e05d4bc655cfe6862002a0bd8337291cdc13e
Brian Korir <briankorir026@gmail.com>
2023-06-20 19:27:45 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,35 @@
 #include "main.h"
+
 /**
- * print_times_table - prints the times table for n.
- * @n: The multiplication table requested.
- * Return: Nothing.
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: The number of times to print the table
  */
 void print_times_table(int n)
 {
-	int i, j, res;
+	int row, col, product;
 
-	if (!(n > 15 || n < 0))
+	if (n < 0 || n > 15)
+		return;
+	for (row = 0; row <= n; row++)
 	{
-		for (i = 0; i <= n; i++)
+		for (col = 0; col <= n; col++)
 		{
-			for (j = 0; j <= n; j++)
+			product = row * col;
+			if (col != 0)
 			{
-				res = (i * j);
-				if (j != 0)
-				{
-					_putchar(',');
+				_putchar(',');
+				if (product < 100)
 					_putchar(' ');
-				}
-				if (res < 10 && j != 0)
-				{
+				if (product < 10)
 					_putchar(' ');
-					_putchar(' ');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 10 && res < 100)
-				{
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 100 && j != 0)
-				{
-					_putchar((res / 100) + '0');
-					_putchar((res / 10) % 10 + '0');
-					_putchar((res % 10) + '0');
-				}
-				else
-					_putchar((res % 10) + '0');
 			}
-			_putchar('\n');
+			if (product >= 100)
+				_putchar((product / 100) + '0');
+			if (product >= 10)
+				_putchar(((product / 10) % 10) + '0');
+			_putchar((product % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
+
