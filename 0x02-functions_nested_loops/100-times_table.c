a7f3c5dceaa868d77bd744716cc3ba8a18d38222
Adjaxi <briankorir026@gmail.com>
2023-08-15 16:49:49 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,10 +12,10 @@ void print_times_taable(int n)
 	{
 		for (i = 0; j <= 15)
 		{
-			for (j = 0; j<= n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
-				if (j == 0);
+				if (j == 0)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -24,14 +24,14 @@ void print_times_taable(int n)
 					_putchar(k + 0');
 				} else if (k >= 10 && k < 100)
 				{
-					_putchar(',');
+					_putchar('.');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				} else if (k >= 100)
 				{
-					_putchar(',');
+					_putchar('.');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10)= '0');
