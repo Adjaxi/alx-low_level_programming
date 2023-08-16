5b7bc31a9a383760fb774884de4ad9ddb1dd8558
Adjaxi <briankorir026@gmail.com>
2023-08-16 16:06:16 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,82 @@
 #include "main.h"
 
 /**
- * print_times_table - prints then n times table, starting with 0
- * @n: number of the times table
+ * print_times_table - This function prints n times table, starting with 0.
+ * @n: The number of column and row
  */
-void print_times_taable(int n)
+void print_times_table(int n)
 {
-	int i, j, k;
+	int i = 0;
+	int j, product;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; j <= 15)
+		while (i <= n)
 		{
-			for (j = 0; j <= n; j++)
+			j = 0;
+			while (j <= n)
 			{
-				k = j * i;
-				if (j == 0)
+				product = i * j;
+				if (product == 0)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + 0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar('.');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar('.');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10)= '0');
-					_putchar((k % 10) + '0');
+					if (j != 0)
+						printSpace(3);
+					_putchar('0');
 				}
+				else if (product < 10)
+					printInt(product);
+				else if (product >= 10 && product < 100)
+					printInt(product);
+				else
+					printInt(product);
+
+				if (j + 1  <= n)
+					_putchar(',');
+				j++;
 			}
 			_putchar('\n');
+			i++;
 		}
+	}
+}
+
+/**
+ * printSpace - print n number of spaces
+ * @n: The number of spaces to be printed
+ */
+void printSpace(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar(' ');
+		i++;
+	}
+}
+
+/**
+ * printInt - print the given int to stdout
+ * @n: the integer to be printed
+ */
+void printInt(int n)
+{
+	if (n < 10)
+	{
+		printSpace(3);
+		_putchar(n + '0');
+	}
+	else if (n >= 10 && n < 100)
+	{
+		printSpace(2);
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		printSpace(1);
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
 	}
 }
